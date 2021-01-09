4f88e326439caa90655b5ce7bba671110e67eb46
Kate <kate755@gmail.com>
2021-01-09 15:28:13 -0800

    reading profile data from firebase

--- hello-world/src/components/Profile.vue
+++ hello-world/src/components/Profile.vue
@@ -1,22 +1,43 @@
 <template>
   <div id="profile">
-    <div class="userInfo">Kate Lu</div>
-    <div class="userInfo">Student</div>
+    <div class="userInfo">{{ userName }}</div>
+    <div class="userInfo">{{ userRole }}</div>
     <b-btn @click="updateProfile">Click to update</b-btn>
   </div>
 </template>
 
 <script>
+import { usersCollection } from "@/firebaseConfig"
+
 export default {
   name: 'Profile',
-
-  components: {
-  },
-
-  methods: {
-    updateProfile() {
-      console.log("update profile for Kate");
+  data() {
+		return {
+      userName: "",
+      userRole: ""
     }
+  },
+  components: {
+    
+  },
+  mounted() {
+    this.fetchProfile();
+  },
+  methods: {
+    fetchProfile() {
+
+      usersCollection.doc('wuDzgF9FuabsbGJ6Pb5S').get().then(doc => {
+        const userInfo = doc.data()
+        this.userName = userInfo.name;
+        this.userRole = userInfo.role;
+      });
+      
+    },
+
+    updateProfile() {
+      console.log("update profile for " + this.userName);
+    },
+
   }
 }
 </script>
