21acf3d9859718a1b7c7a516b8b9c54b728c1d22
Kate <kate755@gmail.com>
2021-01-09 14:12:53 -0800

    basic profile component

---
+++ hello-world/src/components/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="profile">
+    <div class="userInfo">Kate Lu</div>
+    <div class="userInfo">Student</div>
+    <b-btn @click="updateProfile">Click to update</b-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+
+  components: {
+  },
+
+  methods: {
+    updateProfile() {
+      console.log("update profile for Kate");
+    }
+  }
+}
+</script>
+
+<style>
+.userInfo {
+  margin: 0.5rem;
+}
+
+#profile {
+  padding: 2rem;
+  min-width: 800px;
+  background-color: papayawhip;
+}
+</style>
