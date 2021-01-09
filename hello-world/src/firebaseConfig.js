f22e0dd8622bfa76aa0bf93fab1fc7c932d2d18f
Kate <kate755@gmail.com>
2021-01-09 13:35:01 -0800

    linked firebase to project

---
+++ hello-world/src/firebaseConfig.js
@@ -0,0 +1,26 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBH8NKSDEA-TA1PpGNIDhceclp2e-aCsn4",
+  authDomain: "bump-ad819.firebaseapp.com",
+  projectId: "bump-ad819",
+  storageBucket: "bump-ad819.appspot.com",
+  messagingSenderId: "615249473766",
+  appId: "1:615249473766:web:f4b73949990259898dcb27",
+  measurementId: "G-NEC63D82DP"
+};
+
+// utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// collection references
+const usersCollection = db.collection('users')
+
+export {
+  db,
+  auth,
+  usersCollection,
+}
+
+firebase.initializeApp(firebaseConfig);
